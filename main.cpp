cb02e23946659f39e2190a0259cba5675a415938
Devv1507 <davide.valencia.v@gmail.com>
2024-11-04 11:28:25 -0500

    documents: se añadió un ejemplo de como usar la clase Persona, de la forma en que está actualmente

--- main.cpp
+++ main.cpp
@@ -4,5 +4,10 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
+
+    // Direccion direccion("123", "Centro", "Cali", "Valle del Cauca");
+    // Persona juan("Juan Perez", direccion);
+    // juan.mostrarDatos();
+
     return 0;
 }
