41cd510f6a407cde1b13d2a5f7e689998e23d532
jpeskmilla <juan.escamilla@correounivalle.edu.co>
2024-11-09 09:17:06 -0500

    Inicializacion, getters y setters de la clase Proveedor

--- Proveedor.h
+++ Proveedor.h
@@ -1,4 +1,18 @@
 #include <iostream>
 using namespace std;
 
-class Proveedor {};
+class Proveedor {
+    private:
+        string rut, banco, numeroCuenta, tipoCuenta;
+    public:
+        Proveedor(string, string, string, string);
+        string getRut();
+        string getBanco();
+        string getNumeroCuenta();
+        string getTipoCuenta();
+        void setRut(string);
+        void setBanco(string);
+        void setNumeroCuenta(string);
+        void setTipoCuenta(string);
+        void mostrarDatos();
+    };
