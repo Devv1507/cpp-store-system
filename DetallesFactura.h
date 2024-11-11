d2c4c55e4593fa243a263dc19068d4ec44935580
Devv1507 <davide.valencia.v@gmail.com>
2024-11-11 01:48:31 -0500

    se mejoró la implementación de DetallesFactura

--- DetallesFactura.h
+++ DetallesFactura.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include "Producto.h"
 
 using namespace std;
@@ -15,21 +16,35 @@ class DetallesFactura {
          *      subtotalProducto: valor total de la compra de un producto
          */
         DetallesFactura(Producto &producto, int cantidad): producto(producto), cantidad(cantidad) {
+            validarCantidad();
             calcularSubtotal();
         };
         // Getters y Setters
-        int getCantidad() { return cantidad; }
-        float getSubtotalProducto() { return subtotalProducto; }
-        // Métodos específicos
+        int getCantidad() { return cantidad; };
+        float getSubtotalProducto() { return subtotalProducto; };
+        Producto getProducto() const { return producto; };
+        void setCantidad(int cantidad) {
+            this->cantidad = cantidad;
+            validarCantidad();
+            calcularSubtotal();
+        };
+        void setSubtotalProducto(float subtotal) { this->subtotalProducto = subtotal; };
+        /************************************************************ Métodos específicos ****************/
+        // Este método permite validar que la cantidad de productos sea positiva y mayor a 0
+        void validarCantidad() {
+            if (cantidad <= 0) {
+                throw invalid_argument("La cantidad de productos debe ser mayor a 0.");
+            }
+        };
+        // Este método permite calcular el subtotal de la compra de un producto
         void calcularSubtotal() {
             float precioProducto = producto.getPrecioUnitario();
-            subtotalProducto = precioProducto * cantidad;
-            this->subtotalProducto = subtotalProducto;
+            this->subtotalProducto = precioProducto * cantidad;
         };
         // Este método permite imprimir el detalle particular
         void mostrarDetalle() {
             cout << "Producto: " << producto.getNombreProducto()
              << ", Cantidad: " << cantidad
-             << ", Subtotal: $" << subtotalProducto << endl;
+             << ", Subtotal: $" << setprecision(2) << subtotalProducto << endl;
         };
 };
