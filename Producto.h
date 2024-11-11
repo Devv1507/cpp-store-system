0dd35e32f68ab24e3313e88442cebe2efeae2664
Devv1507 <davide.valencia.v@gmail.com>
2024-11-10 22:31:39 -0500

    se añadieron los getters y setters a la clase Producto

--- Producto.h
+++ Producto.h
@@ -1,4 +1,17 @@
 #include <iostream>
 using namespace std;
 
-class Producto {};
+class Producto {
+    private:
+        string idProducto, nombreProducto;
+        float precioUnitario;
+    public:
+        Producto(string idProducto, string nombreProducto, float precioUnitario): idProducto(idProducto), nombreProducto(nombreProducto), precioUnitario(precioUnitario) {};
+        // Getters y Setters
+        string getIdProducto() { return idProducto; };
+        string getNombreProducto() { return nombreProducto; };
+        float getPrecioUnitario() { return precioUnitario; };
+        void setIdProducto(string idProducto) { this->idProducto = idProducto; };
+        void setNombreProducto(string nombreProducto) { this->nombreProducto = nombreProducto; };
+        void setPrecioUnitario(float precioUnitario) { this->precioUnitario = precioUnitario; };
+};
