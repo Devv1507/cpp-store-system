5c0d9d1406af1a7c9410de648439d7a84c2bb5f2
Devv1507 <davide.valencia.v@gmail.com>
2024-11-11 20:23:55 -0500

    se añadió un ejemplo de uso del módulo de facturación actual

--- main.cpp
+++ main.cpp
@@ -9,5 +9,24 @@ int main() {
     // Cliente juan("122346", "Juan Perez", "juan.perez@gmail.com", 3143677337, direccionJuan);
     // juan.mostrarDatos();
 
+    // Proveedor proveedorX("Proveedor X", "proveedorx@example.com", "Calle 50, Bogotá");
+
+    // // Crear una factura de venta
+    // Factura facturaVenta("venta", &juan); // Pasamos el cliente
+    // Producto producto1("Laptop", 1200.50f);
+    // facturaVenta.agregarDetalle(producto1, 1); // Agregar 1 Laptop
+    // facturaVenta.mostrarDatos(); // Mostrar los detalles de la factura
+
+    // // Crear una factura de compra
+    // Factura facturaCompra("compra", &proveedorX); // Pasamos el proveedor
+    // Producto producto2("Mouse", 25.30f);
+    // facturaCompra.agregarDetalle(producto2, 2); // Agregar 2 Mouse
+    // facturaCompra.mostrarDatos(); // Mostrar los detalles de la factura
+
+    // Caja caja("V001");
+    // caja.agregarFactura(facturaVenta);
+    // caja.agregarFactura(facturaCompra);
+    // caja.mostrarFacturas();
+
     return 0;
 }
