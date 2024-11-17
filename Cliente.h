2940e044f716671fa5f9ab7eeff1ae28c940e66e
Devv1507 <davide.valencia.v@gmail.com>
2024-11-17 17:34:20 -0500

    se añadió herencia a los constructores de clases derivadas de Persona

--- Cliente.h
+++ Cliente.h
@@ -7,5 +7,6 @@ class Cliente: public Persona {
     private:
         string idCliente, fechaRegistro;
     public:
-        Cliente(string idCliente, string fechaRegistro, string id, string nombre, string email, int telefono, Direccion &direccion): idCliente(idCliente), fechaRegistro(fechaRegistro), Persona(id, nombre, email, telefono, direccion) {};
+        Cliente(string idCliente, string fechaRegistro, string id, string nombre, string email, int telefono, Direccion &direccion):
+            idCliente(idCliente), fechaRegistro(fechaRegistro), Persona(id, nombre, email, telefono, direccion) {};
     };
--- Proveedor.h
+++ Proveedor.h
@@ -1,11 +1,14 @@
 #include <iostream>
+#include "Persona.h"
+
 using namespace std;
 
-class Proveedor {
+class Proveedor: public Persona {
     private:
         string rut, banco, numeroCuenta, tipoCuenta;
     public:
-        Proveedor(string, string, string, string);
+        Proveedor(string rut, string banco, string numeroCuenta, string tipoCuenta, string id, string nombre, string email, int telefono, Direccion &direccion): 
+            rut(rut), banco(banco), numeroCuenta(numeroCuenta), Persona(id, nombre, email, telefono, direccion) {};
         string getRut();
         string getBanco();
         string getNumeroCuenta();
