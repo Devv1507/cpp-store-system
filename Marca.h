34d20c82b9887c50bd564c1a6a09eb2ddf5dfa89
Nicolle Paz <nicolleePaz16>
2024-11-21 11:58:52 -0500

    Se creó el vector listarProductos y se implemetaron los métodos agregarProducto y mostarProducto

--- Marca.h
+++ Marca.h
@@ -2,12 +2,14 @@
 #include <vector>
 #include <string>
 #include "Stock.h"
+#include "Producto.h"
 using namespace std;
 
 class Marca {
     private:
         int idMarca;
         string nombreMarca;
+        vector<Producto> listarProductos; //vector para guardar los productos de una marca
     public:
         //constructores
         Marca(): idMarca(0), nombreMarca("") {}
@@ -18,7 +20,8 @@ class Marca {
             int setIdMarca();
             string setNombreMarca();
 
-            void listarProductos();
+            void agregarProducto(const Producto& producto);
+            void mostrarProductos();
 };
 
     //definición de getters y setters
@@ -26,3 +29,16 @@ class Marca {
     string Marca::getNombreMarca() {return nombreMarca; }
     int Marca::setIdMarca() { this->idMarca = idMarca; }
     string Marca::setNombreMarca() { this->nombreMarca = nombreMarca; }
+
+    //método para agregar un producto a una marca
+    void Marca::agregarProducto(const Producto& producto) {
+        listarProductos.push_back(producto);
+    }
+
+    //método para mostar todos los productos de una marca
+    void Marca::mostrarProductos() {
+        cout << "Productos de la marca " << nombreMarca << ":" << endl;
+        for (size_t i = 0; i < listarProductos.size(); i++) {
+            listarProductos[i].mostrarDatos();
+        }
+    }
