d2aa3cef7d35441529948c8af34df24cdde9a517
Devv1507 <davide.valencia.v@gmail.com>
2024-11-24 20:44:31 -0500

    se realizaron mejoras a la lógica interna de la clase Caja

--- Caja.h
+++ Caja.h
@@ -1,31 +1,45 @@
 #include <iostream>
-#include <vector>
+#include <iomanip>
+#include <sstream>
 #include <string>
+#include <vector>
 #include "Factura.h"
 
 using namespace std;
 
 class Caja {
     private:
-        vector<Factura> facturas;
-        string idVenta;
+        static int contadorCajas; // Contador estático para generar IDs únicos
+        string idCaja;
         int numeroFacturasDia;
+        vector<Factura> facturas;
+
+        // Método para generar un identificador único basado en un contador
+        string generarIdCaja() {
+            stringstream stringstream;
+            stringstream << "C-" << setw(2) << setfill('0') << contadorCajas++;
+            return stringstream.str();
+        };
     public:
-        Caja(string idVenta): idVenta(idVenta), numeroFacturasDia(0) {};
+        // Constructor
+        Caja(): idCaja(generarIdCaja()), numeroFacturasDia(0) {};
+        // Getters y Setters
+        string getIdCaja() { return idCaja; };
+        int getNumeroFacturasDia() { return numeroFacturasDia; };
+        void setIdCaja(string idCaja) { this->idCaja = idCaja; };
+        void setNumeroFacturasDia(int numeroFacturasDia) { this->numeroFacturasDia = numeroFacturasDia; };
+        /************************************************ Métodos específicos ************************************************/
+        // Este método permite agregar una factura a la caja
         void agregarFactura(Factura factura) {
             facturas.push_back(factura);
             numeroFacturasDia++;
         };
-        // Getters y Setters
-        string getIdVenta() { return idVenta; };
-        int getNumeroFacturasDia() { return numeroFacturasDia; };
-        void setIdVenta(string idVenta) { this->idVenta = idVenta; };
-        void setNumeroFacturasDia(int numeroFacturasDia) { this->numeroFacturasDia = numeroFacturasDia; };
-        /************************************************************ Métodos específicos ****************/
+
          // Método auxiliar para obtener el mes
         int obtenerMes(const string &fecha) {
             return stoi(fecha.substr(5, 2)); // asume el formato YYYY-MM-DD
         };
+
         // Este método permite calcular el total de ventas mensuales
         float calcularTotalVentasMensuales(int mes) {
             float totalVentas = 0;
@@ -36,6 +50,7 @@ class Caja {
             }
             return totalVentas;
         };
+
         // Este método permite imprimir la información de las facturas almacenadas
         void mostrarFacturas() {
             for (size_t i = 0; i < facturas.size(); i++) {
@@ -43,6 +58,7 @@ class Caja {
                 facturas[i].mostrarDatos();
             }
         };
+
         // Este método permite listar las facturas por tipo de venta
         void listarFacturasPorTipo(string tipoFactura) {
             cout << "--- Facturas del tipo: " << tipoFactura << " ---" << endl;
