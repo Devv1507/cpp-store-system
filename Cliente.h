25e1b9180edf9aca812220afe01776786819bb6d
Devv1507 <davide.valencia.v@gmail.com>
2024-11-24 20:38:25 -0500

    se removio la propiedad totalCompras de la clase Cliente para manejar esto en Tienda

--- Cliente.h
+++ Cliente.h
@@ -7,26 +7,22 @@ using namespace std;
 class Cliente : public Persona {
     private:
         string rut, profesion, tipoCliente;
-        float totalCompras;
     public:
         Cliente(string id, string nombre, string email, int telefono, Direccion& direccion, string rut, string profesion, string tipoCliente, float totalCompras):
-            Persona(id, nombre, email, telefono, direccion), rut(rut), profesion(profesion), tipoCliente(tipoCliente), totalCompras(totalCompras) {}
+            Persona(id, nombre, email, telefono, direccion), rut(rut), profesion(profesion), tipoCliente(tipoCliente) {}
         //Getters y Setters
         string getRut(){ return rut; };
         string getProfesion() { return profesion; };
         string getTipoCliente() { return tipoCliente; };
-        float getTotalCompras() { return totalCompras; };
         void setRut(string rut) { this->rut = rut; };
         void setProfesion(string profesion) { this->profesion = profesion; };
         void setTipoCliente(string tipoCliente) { this->tipoCliente = tipoCliente; };
-        void setTotalCompras(float totalCompras) { this->totalCompras = totalCompras; };
         //Método para mostrar los datos completos del cliente
         void mostrarDatos() override {
             Persona::mostrarDatos();
             cout << left 
                 << setw(10) << "RUT: " << rut << endl
                 << setw(10) << "Profesión: " << profesion << endl
-                << setw(10) << "Tipo de Cliente: " << tipoCliente << endl
-                << setw(10) << "Total de Compras: " << totalCompras << endl;
+                << setw(10) << "Tipo de Cliente: " << tipoCliente << endl;
         };
 };
