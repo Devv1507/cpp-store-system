d014e53c6ada06fa6c2e2b6fe8b146d4796acdf8
Devv1507 <davide.valencia.v@gmail.com>
2024-12-09 00:39:55 -0500

    se elimino el setSutotalProducto duplicado

--- DetallesFactura.h
+++ DetallesFactura.h
@@ -10,27 +10,6 @@ class DetallesFactura {
         Producto producto; // Objeto de la clase Producto que contiene los datos del producto
         int cantidad; // Cantidad de productos comprados
         float subtotalProducto; // Valor total de la compra de un producto
-    public:
-        /**
-         * @brief Constructor de la clase DetallesFactura.
-         * @param producto Referencia a un objeto de la clase Producto.
-         * @param cantidad Cantidad de productos comprados.
-         */
-        DetallesFactura(Producto &producto, int cantidad): producto(producto), cantidad(cantidad) {
-            validarCantidad();
-            calcularSubtotal();
-        };
-        
-        // Getters y Setters
-        int getCantidad() { return cantidad; };
-        float setSubtotalProducto() { return subtotalProducto; };
-        Producto getProducto() const { return producto; };
-        void setCantidad(int cantidad) {
-            this->cantidad = cantidad;
-            validarCantidad();
-            calcularSubtotal();
-        };
-        void setSubtotalProducto(float subtotal) { this->subtotalProducto = subtotal; };
 
         /**
          * @brief Valida que la cantidad de productos sea positiva y mayor a 0.
@@ -48,6 +27,29 @@ class DetallesFactura {
             float precioProducto = producto.getPrecioUnitario();
             this->subtotalProducto = precioProducto * cantidad;
         };
+    public:
+        /**
+         * @brief Constructor de la clase DetallesFactura.
+         * @param producto Referencia a un objeto de la clase Producto.
+         * @param cantidad Cantidad de productos comprados.
+         */
+        DetallesFactura(Producto &producto, int cantidad): producto(producto), cantidad(cantidad) {
+            validarCantidad();
+            calcularSubtotal();
+        };
+        
+        // Getters
+        int getCantidad() { return cantidad; };
+        float getSubtotalProducto() { return subtotalProducto; };
+        Producto getProducto() const { return producto; };
+
+        // Setters
+        void setCantidad(int cantidad) {
+            this->cantidad = cantidad;
+            validarCantidad();
+            calcularSubtotal();
+        };
+        void setSubtotalProducto(float subtotal) { this->subtotalProducto = subtotal; };
 
         /**
          * @brief Imprime el detalle particular.
